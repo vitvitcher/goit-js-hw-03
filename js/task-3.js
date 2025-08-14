0b953219fd71fe2a8503b431eb260e00b0711a86
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2025-08-15 02:20:53 +0300

    3 for 3

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+'use strict';
+function filterArray(numbers, value) {
+    let arr = [];
+    for (const num of numbers) {
+        if (num > value) {
+            arr.push(num)
+        }
+    }
+    return arr;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
